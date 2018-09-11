87c955bdbcdae13f7b234201f45bb832157164af
DESKTOP-C7ND9C0\proitzen <lakmal@proitzen.com>
2018-09-11 10:13:08 +0530

    update

--- app/src/main/java/com/udayalakmal/glideimagerotate/MainActivity.java
+++ app/src/main/java/com/udayalakmal/glideimagerotate/MainActivity.java
@@ -1,5 +1,19 @@
 package com.udayalakmal.glideimagerotate;
+/*
+Copyright 2018 Udaya Lakmal Rathnasiri
 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 import android.graphics.Bitmap;
 import android.graphics.Matrix;
 import android.support.annotation.NonNull;
@@ -17,7 +31,7 @@ import com.bumptech.glide.request.transition.Transition;
 public class MainActivity extends AppCompatActivity {
 
     private Bitmap theBitmap;
-    ImageView iv;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,14 +88,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public static Bitmap RotateBitmap(Bitmap source, float angle)
+    public Bitmap RotateBitmap(Bitmap source, float angle)
     {
         Matrix matrix = new Matrix();
         matrix.postRotate(angle);
         return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
     }
 
-    Bitmap flip(Bitmap source)
+    public Bitmap flip(Bitmap source)
     {
         Matrix m = new Matrix();
         m.preScale(-1, 1);
